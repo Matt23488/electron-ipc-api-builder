745a568327813602f4225c3c0247dd61573de0fa
Matthew Bryant <matt23488@gmail.com>
2022-11-30 10:31:42 -0500

    Fixed export issue

--- src/ipc/getApi.ts
+++ src/ipc/getApi.ts
@@ -1,6 +1,6 @@
 import { ApiDescriptor } from './ApiDescriptor';
 
-const getApi = <
+export const getApi = <
   Name extends string,
   Methods extends string,
   MethodsApi extends Record<Methods, unknown[]>,
@@ -8,6 +8,4 @@ const getApi = <
   MessagesApi extends Record<Messages, unknown[]>,
 >(
   api: ApiDescriptor<Name, Methods, MethodsApi, Messages, MessagesApi>,
-) => (window as typeof api._windowExtension)[api.name]; // as typeof api._windowExtension[Name];
-
-export default getApi;
+) => (window as typeof api._windowExtension)[api.name];
